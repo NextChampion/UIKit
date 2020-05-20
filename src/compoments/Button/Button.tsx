5713dd203dd3ebca29dd89d60bfcf6ddb84cbcb0
zhangcx01 <zhangcx01@mail.jj.cn>
2020-05-20 23:26:13 +0800

    添加titleStyle属性

--- src/compoments/Button/Button.tsx
+++ src/compoments/Button/Button.tsx
@@ -5,23 +5,27 @@ import {
   TouchableOpacity,
   ViewStyle,
   ViewProps,
+  TextStyle,
 } from 'react-native';
+import {Color} from 'Theme';
 
 export interface BtnProps extends ViewProps {
   title?: string;
   disabled?: boolean;
   onPress?: () => void;
-  type?: 'primary' | 'normal' | 'ghost';
+  type?: 'primary' | 'danger' | 'ghost';
   block?: boolean;
   shape?: 'normal' | 'circle';
   size?: 'large' | 'normal' | 'small';
   style?: ViewStyle;
+  titleStyle?: TextStyle;
   children?: ReactNode;
 }
 
 function Button(props: BtnProps) {
   const {
     title,
+    titleStyle,
     disabled,
     type,
     block,
@@ -43,7 +47,7 @@ function Button(props: BtnProps) {
       disabled={disabled}
       style={containetStyle}
       onPress={onPress}>
-      {children || <Text> {title} </Text>}
+      {children || <Text style={[styles.title, titleStyle]}> {title} </Text>}
     </TouchableOpacity>
   );
 }
@@ -52,7 +56,7 @@ Button.defaultProps = {
   title: '',
   disabled: false,
   onPress: null,
-  type: 'normal',
+  type: 'primary',
   block: false,
   shape: 'normal',
   size: 'normal',
@@ -67,11 +71,12 @@ interface ButtonStyles {
   normalSize: ViewStyle;
   smallSize: ViewStyle;
   primaryType: ViewStyle;
-  normalType: ViewStyle;
+  dangerType: ViewStyle;
   ghostType: ViewStyle;
   normalShape: ViewStyle;
   circleShape: ViewStyle;
   block: ViewStyle;
+  title: TextStyle;
   [propName: string]: ViewStyle;
 }
 const styles = StyleSheet.create<ButtonStyles>({
@@ -92,10 +97,10 @@ const styles = StyleSheet.create<ButtonStyles>({
     alignSelf: 'center',
   },
   primaryType: {
-    backgroundColor: 'green',
+    backgroundColor: Color.primary,
   },
-  normalType: {
-    backgroundColor: 'red',
+  dangerType: {
+    backgroundColor: Color.danger,
   },
   ghostType: {
     backgroundColor: 'white',
@@ -110,5 +115,10 @@ const styles = StyleSheet.create<ButtonStyles>({
   },
   block: {
     width: '100%',
+  },
+  title: {
+    alignSelf: 'center',
+    alignContent: 'center',
+    color: Color.white,
   },
 });
